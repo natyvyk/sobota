4e408bb7c980690d8dea0e31a96f45eca93f90db
Natalie Vykoukalova <naty.vykoukalova@gmail.com>
2024-10-12 21:02:21 +0200

    olalaaa

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-document.body.innerHTML += "Délka filmu : "
-document.body.innerHTML += Math.floor (223 / 60) + " hodiny "
-document.body.innerHTML += 223 % 60 + " minut"
+document.body.innerHTML += "natalie" + "." + "vykoukalova" + "@mujmail.com"
