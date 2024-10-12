e1b4c4d87b1163f23e9fa7de83b1924c927acd2b
Natalie Vykoukalova <naty.vykoukalova@gmail.com>
2024-10-12 20:59:22 +0200

    filmik

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.body.innerHTML += "Měsíční výplata" + " = "
-document.body.innerHTML += 7 * 320 * 21
-document.body.innerHTML += "<br>" + " Daň = "
-document.body.innerHTML += Math.floor (7 * 320 * 21 * (1-0.6) * 0.15)
+document.body.innerHTML += "Délka filmu : "
+document.body.innerHTML += Math.floor (223 / 60) + " hodiny "
+document.body.innerHTML += 223 % 60 + " minut"
